cbae19b9fc023d83788226bb8403cfc663e5eb15
qandeeil <bb.qandeeil@gmail.com>
2022-07-16 19:54:47 +0300

    React

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <nav>
+            <h2>Cascade</h2>
+            <ul>
+                <li><a href='#'>Home</a></li>
+                <li><a href='#'>Services</a></li>
+                <li><a href='#'>Gallery</a></li>
+                <li><a href='#'>About</a></li>
+                <li><a href='#'>Contact</a></li>
+            </ul>
+        </nav>
+        <div className='content'>
+            <h1>Webdev Creations</h1>
+            <a href='#'>Watch Video</a>
+            <a href='#'>Explore More</a>
+        </div>
+    </div>
+  );
+}
+
+export default App;
